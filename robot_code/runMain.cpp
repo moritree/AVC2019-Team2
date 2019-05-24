6a1765f95db92bdbe4fbbf62237f1142f8f267f9
Zuni <zuni.zkep@gmail.com>
2019-05-24 04:49:47 +0000

    na

---
+++ robot_code/runMain.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Zuni Preece on 2019-05-23.
+//
+
+#include <iostream>
+#include "include/robot.h"
+#include "include/readCamera.h"
+#include "include/drive.h"
+
+int main() {
+    readCamera rc;
+    drive dr;
+    robot rb(rc, dr);
+
+    rb.quadrant1();
+    rb.quadrant2();
+    return 0;
+}
