08eedc210884167a21c853e7c663e267f3db2c56
zuni <zuni.zkep@gmail.com>
2019-05-31 17:59:01 +1200

    fix

--- LibE101/libtest/runMain.cpp
+++ LibE101/libtest/runMain.cpp
@@ -278,7 +278,7 @@ public:
         while (!cam.isLine(array, 0.3)) {
             cam.simplePixelsFromCamera(3, array, 230, 70);
             dri.turn(3, 0);
-            std::this_thread::sleep_for(std::chrono::milliseconds(600));
+            std::this_thread::sleep_for(std::chrono::milliseconds(300));
             printf("turn\n");
         }
         quadrant3();
