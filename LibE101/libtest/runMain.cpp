15f245c4344f2e7a5659b1aac9fe7a54ffb2c5d3
zuni <zuni.zkep@gmail.com>
2019-05-31 17:42:57 +1200

    fix

--- LibE101/libtest/runMain.cpp
+++ LibE101/libtest/runMain.cpp
@@ -271,7 +271,7 @@ public:
         }
         printf("END LINE\n");
         while (!cam.isLine(array, 0.3)) {
-            dri.turn(5, -3);
+            dri.turn(2, -3);
             std::this_thread::sleep_for(std::chrono::milliseconds(200));
             printf("turn\n");
         }
